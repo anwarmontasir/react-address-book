a89047c965e399f3aabc6ac92ebee2d306b0ede1
Anwar Montasir <anwarmontasir@gmail.com>
2018-11-29 15:26:27 -0800

    address book and contact components

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,3 +1,17 @@
 import React from 'react';
 
 import './contact.css';
+
+export default function Contact(props) {
+    return (
+        <div id={`contact-${props.index}`} className="contact">
+            <img src={props.photo} alt={props.name} className="contact-photo" />
+            <h2 className="contact-name">
+                {props.name}
+            </h2>
+            <div className="contact-address">
+                {props.address}
+            </div>
+        </div>
+    )
+}
